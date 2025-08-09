d590942762321140c16cf45ae27f425808be6a40
Anton Müller <antxn.07@gmail.com>
2025-08-09 20:42:26 +0200

    Added catppuccin setup config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,23 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+        require("catppuccin").setup(
+            {
+                -- latte/frappe/macchiato/mocha
+                flavour = "mocha",
+                transparent_background = false,
+                term_colors = true,
+                styles = {
+                    comments = { "italic" },
+                    keywords = {},
+                },
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    telescope = true,
+                    neotree = true,
+                },
+            })
         vim.cmd.colorscheme("catppuccin")
     end,
 }
