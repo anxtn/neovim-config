c9b33d8cf88f06c82437c534e1c9cde32507d59f
Anton Müller <antxn.07@gmail.com>
2025-08-09 19:30:13 +0200

    Added auto code formatting, hover diagnostics and go to definition keymap

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,3 +2,45 @@ vim.lsp.enable({
     "clangd",
     "lua_ls",
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local capabilities = client and client.server_capabilities
+
+        if capabilities and capabilities.definitionProvider then
+            vim.keymap.set(
+                "n",
+                "gd",
+                vim.lsp.buf.definition,
+                { buffer = bufnr, noremap = true, silent = true, desc = "LSP: go to definition" }
+            )
+        end
+
+        if capabilities and capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = bufnr,
+                callback = function()
+                    if client then
+                        vim.lsp.buf.format({ bufnr = bufnr, id = client.id })
+                    end
+                end
+            })
+        end
+
+        vim.api.nvim_create_autocmd("CursorHold", {
+            buffer = bufnr,
+            callback = function()
+                local diagnostics = vim.diagnostic.get(bufnr, { lnum = vim.fn.line(".") - 1 })
+                if #diagnostics > 0 then
+                    vim.diagnostic.open_float(nil, {
+                        focus = false,
+                        scope = "cursor",
+                        border = "rounded",
+                    })
+                end
+            end,
+        })
+    end,
+})
