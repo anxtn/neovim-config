3dee300e63330b69c959aac5847c18cfa946a893
Anton Müller <antxn.07@gmail.com>
2025-08-23 13:18:13 +0200

    Changed Symbole Naming

--- test/test.pas
+++ test/test.pas
@@ -9,20 +9,20 @@ type
   end;
 
 var
-  person: TPerson;
-  i: Integer;
+  Person: TPerson;
+  I: Integer;
 
-function Add(a, b: Integer): Integer;
+function Add(A, B: Integer): Integer;
 begin
-  Result := a + b;
+  Result := A + B;
 end;
 
 begin
-  person.ID := 1;
-  person.Name := 'Tom';
+  Person.ID := 1;
+  Person.Name := 'Tom';
 
-  for i := 1 to 5 do
+  for I := 1 to 5 do
   begin
-    WriteLn('Hello ', person.Name, '! ', i, ' + 2 = ', Add(i, 2));
+    WriteLn('Hello ', Person.Name, '! ', I, ' + 2 = ', Add(i, 2));
   end;
 end.
